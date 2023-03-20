202c08f434ff3296616ec7157dbd640659c0b0ef
richardson-miranda <128246475+richardson-miranda@users.noreply.github.com>
2023-03-19 20:49:06 -0400

    Add files via upload

---
+++ WalmartSalesProject.sql
@@ -0,0 +1,34 @@
+
+Select * 
+From Walmart$
+
+--Which week had the highest sales?
+Select date, MAX(Weekly_Sales) as WeeklySales
+From Walmart$
+Group by date
+Order by MAX(Weekly_Sales) DESC
+-- OUTPUT: 24-12-2010 | 3818686.45
+
+
+--How was the weather during the week of highest sales?
+Select date, Temperature, Weekly_Sales
+From Walmart$
+Order by Weekly_Sales DESC
+-- OUTPUT: 30.59
+
+--What was the weather during the lowest weekly sales?
+Select Temperature, Weekly_Sales
+From Walmart$
+Order by Weekly_Sales 
+-- OUTPUT: 52.82
+
+--What was the weekly sales at the lowest temperature?
+Select Temperature, Weekly_Sales
+From Walmart$
+Order by Temperature ASC
+-- OUTPUT: -2.06 | 558027.77
+
+
+--Conclude whether the weather has an essential impact on sales.
+	-- The top 45 sales weeks are in the months of November and December,
+	-- indicating that the holiday season has an impact on sales
